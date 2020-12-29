1bcf03ab36fea18c72f1f41fde0638db90177566
Shaw Haines <haohaistone@qq.com>
2020-12-29 23:32:26 +0800

    Wrote my first phong shader.

--- src/main.js
+++ src/main.js
@@ -1,64 +1,114 @@
-import * as twgl from "../modules/twgl-full.module"
-// vertex shader
-const vs =
-`attribute vec4 a_Position;
-//uniform mat4 proj;
-//varying vec4 a_color;
-//attribute vec4 g_color;
-uniform float time;
-uniform float resolution[2];
-void main() {
-// setting the coordinates
+const vs = `
+attribute vec3 aPos;
+attribute vec3 aNormal;
 
-gl_Position = a_Position;
-if(resolution[0] > resolution[1])
-    gl_Position.x *= resolution[1] / resolution[0];
-else
-    gl_Position.y *= resolution[1] / resolution[0];
-gl_Position += vec4(time, 0, 0, 0);
-//a_color = g_color;
-}`;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
-// fragment shader
-const fs =
-`precision mediump float; // Adjusting precision
-varying vec4 a_color;
-void main() {
-// setting color
-//gl_FragColor = a_color;
-gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-}`;
-
-window.onload = function ()
+varying vec3 Normal;
+varying vec3 fragPos;
+void main()
 {
-    const gl = document.getElementById("c").getContext("webgl");
-    if (!gl) {
-        console.log("Failed");
-        return;
-    }
-    const programInfo = twgl.createProgramInfo(gl, [vs, fs]);
-
-    const arrays = {
-        a_Position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
-    };
-    const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
-    //console.log(twgl)
-    function render(time) {
-        twgl.resizeCanvasToDisplaySize(gl.canvas);
-        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    
-        const uniforms = {
-        time: time * 0.001,
-        resolution: [gl.canvas.width, gl.canvas.height],
-        };
-    
-        gl.useProgram(programInfo.program);
-        twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
-        twgl.setUniforms(programInfo, uniforms);
-        twgl.drawBufferInfo(gl, gl.TRIANGLES, bufferInfo);
-        //console.log("hello!");
-        requestAnimationFrame(render);
-    }
-    requestAnimationFrame(render);    
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    Normal = aNormal;
+    fragPos = (model * vec4(aPos, 1.0)).xyz;
 }
+`
+const fs =`
+precision highp float;
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
 
+varying vec3 Normal;
+varying vec3 fragPos;
+void main()
+{
+    float ambientStrength = 0.5;
+    vec3 ambient = ambientStrength * lightColor;
+
+    vec3 lightDir = normalize(lightPos - fragPos);
+    float diff = max(dot(lightDir, normalize(Normal)),0.0);
+    vec3 diffuse = lightColor * diff;
+
+    vec3 viewDir = normalize(viewPos - fragPos);
+    vec3 reflectDir = reflect(-lightDir, normalize(Normal));
+    // // 32 times
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32.0);
+    vec3 specular = lightColor * spec * 0.5;
+
+    vec3 result = (ambient + diffuse + specular)* objectColor;
+    // vec3 result = (ambient + diffuse)* objectColor;
+    gl_FragColor = vec4(result, 1.0);
+}`;
+
+var camera_x = 0;
+var camera_y = 0;
+var camera_z = 0;
+
+import * as twgl from '../modules/twgl-full.module.js';
+const m4 = twgl.m4;
+const gl = document.getElementById("c").getContext("webgl");
+if (!gl) console.log("Failed");
+const programInfo = twgl.createProgramInfo(gl, [vs, fs]);
+
+//Both ways work fine!
+const arrays = {
+    aPos:     [1, 1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, -1, 1, 1, -1, 1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, -1, 1, -1, 1, 1, -1, 1, -1, -1, -1, -1, -1],
+    aNormal:   [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1],
+    // texcoord: [1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1],
+    indices:  [0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23],
+};
+// const arrays = twgl.primitives.createCubeVertices(2);
+const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+console.log(arrays)
+console.log(bufferInfo)
+
+const uniforms = {
+    objectColor:[0.8,0.8,0.5],
+    lightColor:[1.0,1.0,1.0],
+    lightPos:[0.,3.,5],
+};
+
+function render(time) {
+    time *= 0.001;
+    twgl.resizeCanvasToDisplaySize(gl.canvas);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    const fov = 30 * Math.PI / 180;
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.5;
+    const zFar = 10;
+    const projection = m4.perspective(fov, aspect, zNear, zFar);
+    const eye = [1 + camera_x, 4 + camera_y, -6 + camera_z];
+    const target = [0 + camera_x, 0 + camera_y, 0 + camera_z];
+    const up = [0, 1, 0];
+
+    const camera = m4.lookAt(eye, target, up);
+    const view = m4.inverse(camera);
+    const viewProjection = m4.multiply(projection, view);
+    const world = m4.rotationY(time);
+
+    uniforms.view = view;
+    uniforms.model = world;
+    uniforms.projection = projection;
+    uniforms.viewPos=eye;
+    // uniforms.u_worldViewProjection = m4.multiply(viewProjection, world);
+
+    gl.useProgram(programInfo.program);
+    twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+    twgl.setUniforms(programInfo, uniforms);
+
+    //All 3 ways work fine!
+    twgl.drawBufferInfo(gl, bufferInfo, gl.TRIANGLES, bufferInfo.numelements);
+    // twgl.drawBufferInfo(gl, bufferInfo, gl.TRIANGLES);
+    // gl.drawElements(gl.TRIANGLES, bufferInfo.numElements, gl.UNSIGNED_SHORT, 0);
+    requestAnimationFrame(render);
+}
+requestAnimationFrame(render);
