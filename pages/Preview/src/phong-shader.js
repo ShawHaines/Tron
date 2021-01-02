433217643618cef2306ad900714c916678bc21d4
Shaw Haines <haohaistone@qq.com>
2021-01-02 20:02:15 +0800

    Made the two shaders compatible.

--- pages/Preview/src/phong-shader.js
+++ pages/Preview/src/phong-shader.js
@@ -1,46 +1,45 @@
 const vs =`
-attribute vec3 position;
-attribute vec3 normal;
+attribute vec3 a_position;
+attribute vec3 a_normal;
 
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
+uniform mat4 u_worldViewProjection;
+uniform mat4 u_world;
 
 varying vec3 v_normal;
 varying vec3 fragPos;
 void main(){
-    gl_Position = projection * view * model * vec4(position, 1.0);
-    v_normal = normal;
-    fragPos = (model * vec4(position, 1.0)).xyz;
+    gl_Position = u_worldViewProjection * vec4(a_position, 1.0);
+    v_normal = (u_world * vec4(a_normal,0.0)).xyz;
+    fragPos = (u_world * vec4(a_position, 1.0)).xyz;
 }`
 
 const fs=`
 precision highp float;
-uniform vec3 objectColor;
-uniform vec3 lightColor;
-uniform vec3 lightPos;
-uniform vec3 viewPos;
+uniform vec4 u_objectColor;
+// actually it's lightColor, just to be conform with the other shaders.
+uniform vec3 u_ambientLight;
+uniform vec3 u_lightPos;
+uniform vec3 u_viewPos;
 
 varying vec3 v_normal;
 varying vec3 fragPos;
 void main()
 {
     float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
+    vec3 ambient = ambientStrength * u_ambientLight;
 
-    vec3 lightDir = normalize(lightPos - fragPos);
+    vec3 lightDir = normalize(u_lightPos - fragPos);
     float diff = max(dot(lightDir, normalize(v_normal)),0.0);
-    vec3 diffuse = lightColor * diff;
+    vec3 diffuse = u_ambientLight * diff;
 
-    vec3 viewDir = normalize(viewPos - fragPos);
+    vec3 viewDir = normalize(u_viewPos - fragPos);
     vec3 reflectDir = reflect(-lightDir, normalize(v_normal));
     // 32 times
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32.0);
-    vec3 specular = lightColor * spec * 0.5;
+    vec3 specular = u_ambientLight * spec * 0.5;
 
-    vec3 result = (ambient + diffuse + specular)* objectColor;
+    gl_FragColor= vec4((ambient + diffuse + specular),1.0)* u_objectColor;
 
-    gl_FragColor = vec4(result, 1.0);
 }
 `
 export {vs, fs};
--- pages/Preview/src/texture-shader.js
+++ pages/Preview/src/texture-shader.js
@@ -27,53 +27,53 @@ const vs = `
     `;
 
 const fs =
-    `precision mediump float;
-    varying vec2 v_texcoord;
-    varying vec4 v_position;
-    varying vec4 v_realPosition;
+`precision mediump float;
+varying vec2 v_texcoord;
+varying vec4 v_position;
+varying vec4 v_realPosition;
+
+varying vec3 v_normal;
+varying vec3 v_fragPos;
+
+uniform vec4 u_objectColor;
+uniform sampler2D u_texture;
+uniform vec3 u_lightPos;
+uniform vec3 u_viewPos;
+uniform vec3 u_ambientLight;
+uniform vec3 u_diffuseLight;
+uniform vec3 u_specularLight;
+uniform float u_ambientStrength;
+uniform float u_shininess;
+
+
+void main() {
     
-    varying vec3 v_normal;
-    varying vec3 v_fragPos;
+    // vec3 lightColor = vec3(1.0, 1.0, 1.0); //debug use
+    // float ambientStrength = 0.5; //debug use
 
-    uniform vec4 u_objectColor;
-    uniform sampler2D u_texture;
-    uniform vec3 u_lightPos;
-    uniform vec3 u_viewPos;
-    uniform vec3 u_ambientLight;
-    uniform vec3 u_diffuseLight;
-    uniform vec3 u_specularLight;
-    uniform float u_ambientStrength;
-    uniform float u_shininess;
-    
+    vec3 normal = normalize(v_normal);
+    vec3 lightDir = normalize(u_lightPos - v_fragPos);
+    vec3 viewDir = normalize(u_viewPos - v_fragPos);
 
-    void main() {
-        
-        // vec3 lightColor = vec3(1.0, 1.0, 1.0); //debug use
-        // float ambientStrength = 0.5; //debug use
-
-        vec3 normal = normalize(v_normal);
-        vec3 lightDir = normalize(u_lightPos - v_fragPos);
-        vec3 viewDir = normalize(u_viewPos - v_fragPos);
-
-        //Ambient
-        vec3 ambient = u_ambientStrength * u_ambientLight;
-        //Diffuse
-        // float diff = max(dot(lightDir, normal),0.0);
-        float diff = max(dot(lightDir, normal), -dot(lightDir, normal));
-        vec3 diffuse = u_diffuseLight * diff;
-        //Specular
-        vec3 reflectDir = reflect(-lightDir, normal);
-        // float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32.0);
-        float spec = pow(max(dot(viewDir, reflectDir), -dot(viewDir, reflectDir)), u_shininess);
-        vec3 specular = u_specularLight * spec * 0.5;
-        // vec3 specular = vec3(0, 0, 0);
+    //Ambient
+    vec3 ambient = u_ambientStrength * u_ambientLight;
+    //Diffuse
+    // float diff = max(dot(lightDir, normal),0.0);
+    float diff = max(dot(lightDir, normal), -dot(lightDir, normal));
+    vec3 diffuse = u_diffuseLight * diff;
+    //Specular
+    vec3 reflectDir = reflect(-lightDir, normal);
+    // float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32.0);
+    float spec = pow(max(dot(viewDir, reflectDir), -dot(viewDir, reflectDir)), u_shininess);
+    vec3 specular = u_specularLight * spec * 0.5;
+    // vec3 specular = vec3(0, 0, 0);
 
 
-        vec4 texColor = texture2D(u_texture, v_texcoord);
-        vec4 light = vec4((ambient + diffuse + specular), 1.0);
-        vec4 color = texColor + u_objectColor;
-        vec4 result = light * color;
+    vec4 texColor = texture2D(u_texture, v_texcoord);
+    vec4 light = vec4((ambient + diffuse + specular), 1.0);
+    vec4 color = texColor + u_objectColor;
+    vec4 result = light * color;
 
-        gl_FragColor = result;
-    }`;
+    gl_FragColor = result;
+}`;
 export {vs, fs};
