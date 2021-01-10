24eb3b54e8488e0c0ec2a2a75b1437122e131092
vtu <vtu81@github.com>
2021-01-10 22:25:09 +0800

    use UI to control sun light direction :)

--- src/ui.js
+++ src/ui.js
@@ -1,9 +1,15 @@
 
 var display_value = 0;
+window.sunAngle = 90;
+window.objSize = 10;
 // Setup a ui.
-webglLessonsUI.setupSlider("#Light", {value: display_value, slide: updateRotation, min: 0, max: 100});
-webglLessonsUI.setupSlider("#OBJ-size", {value: display_value, slide: updateRotation, min: 0, max: 100});
+webglLessonsUI.setupSlider("#Light", {value: window.sunAngle, slide: updateLightAngle, min: 0, max: 180});
+webglLessonsUI.setupSlider("#OBJ-size", {value: window.objSize, slide: updateObjectSize, min: 1, max: 100});
 
-function updateRotation(event, ui) {
-    display_value = ui.value;
+function updateLightAngle(event, ui) {
+    window.sunAngle = ui.value;
+}
+
+function updateObjectSize(event, ui) {
+    window.objSize = ui.value;
 }
